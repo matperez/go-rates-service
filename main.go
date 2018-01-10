f1c518d5eb268bd9fff9b7dc5c5156e87ee470a6
Andrey Golovin <matperez@mail.ru>
2018-01-11 04:58:49 +0500

    minor changes

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	httpTransport "github.com/go-kit/kit/transport/http"
 	"github.com/matperez/cbr-http-service/cache"
@@ -15,13 +19,11 @@ var (
 )
 
 func init() {
-	flag.StringVar(&httpListening, "L", ":8080", "HTTP port")
+	flag.StringVar(&httpListening, "L", ":8080", "HTTP host and port to listen at")
 	flag.Parse()
 }
 
 func main() {
-	log.Println("Started...")
-
 	svc := NewService(cbr.NewClient(), cache.New())
 
 	getRateHandler := httpTransport.NewServer(
@@ -31,7 +33,21 @@ func main() {
 	)
 
 	http.Handle("/get-rate", getRateHandler)
-	log.Fatal(http.ListenAndServe(httpListening, nil))
 
-	log.Println("Stoped...")
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+
+	go stopper(gracefulStop)
+
+	log.Fatal(http.ListenAndServe(httpListening, nil))
+}
+
+// function to perform a graceful shutdown
+func stopper(gracefulStop chan os.Signal) {
+	sig := <-gracefulStop
+	log.Printf("Caught sig: %+v", sig)
+	log.Println("Wait for 2 seconds to finish processing")
+	time.Sleep(2 * time.Second)
+	os.Exit(0)
 }
